78ff68b3fc1a307c1230239dcd18bde5982f3004
Zaid <solankizaid63@gmail.com>
2026-04-10 01:04:36 +0530

    ModelEvaluation

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+# ==============================
+# Zaid → Evaluation
+# ==============================
+
+print("\nModel Evaluation:")
+print("MAE:", mean_absolute_error(y_test, y_pred))
+print("MSE:", mean_squared_error(y_test, y_pred))
+print("R2 Score:", r2_score(y_test, y_pred))
