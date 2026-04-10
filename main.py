0592648e1fe0037633d6e9ef3aaadc7b2ea96264
Parth <parthsurve111007@gmail.com>
2026-04-10 11:27:10 +0530

    Train-Test split

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
 # ==============================
-# Member 4 → Train-Test Split (START)
+# Member 4 → Train-Test Split
 # ==============================
 
 X_train, X_test, y_train, y_test = train_test_split(
