6849b1b47059106c8872955323fc3e326c84f597
Aryan <uezlyvr@gmail.com>
2026-04-10 10:53:18 +0530

    scaling

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+# ==============================
+# 📏 Member 3 → Scaling 
+# ==============================
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X = pd.DataFrame(X_scaled, columns=X.columns)
+
+print("\nAfter Scaling:")
+print(X.head())
